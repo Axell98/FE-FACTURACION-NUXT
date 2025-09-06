902b45ee4fce2958d435d67cef759d6989ecc7dd
Axell98 <axellvalle.98@gmail.com>
2025-09-05 20:30:45 -0500

    Ajustes

--- components/sucursal/TabFormSeries.vue
+++ components/sucursal/TabFormSeries.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-const accordionSelect = ref([false, false, false, false]);
+const accordionSelect = ref([true, false, false, false]);
 
 const showAccordion = (index: number) => {
 	accordionSelect.value = [false, false, false, false];
@@ -17,17 +17,61 @@ const showAccordion = (index: number) => {
 			:header-icon-color="'text-primary'"
 			:open-accordion="showAccordion"
 		>
-			Facturas y boletas
+			<div class="pb-4">
+				<div class="flex gap-3 py-3 px-4 text-xs text-[#035C9A] bg-[#E6F5FF] rounded-lg">
+					<UIcon
+						name="i-lucide-info"
+						class="size-6"
+					/>
+					<div>
+						<p class="mb-1">
+							Formato de series según SUNAT:
+						</p>
+						<ul class="list-disc ml-4 mb-1 space-y-0.5">
+							<li>
+								<p>Facturas: F + 3 dígitos (Ej: F001)</p>
+							</li>
+							<li>
+								<p>Boletas: B + 3 dígitos (Ej: B001)</p>
+							</li>
+						</ul>
+						<p>Nota: Las series y correlativos son únicos por sucursal.</p>
+						<p>Si creas otra sucursal, cambia el número: F001 -> F002, B001 -> B002, etc. </p>
+					</div>
+				</div>
+			</div>
 		</BaseAccordionCustom>
 		<BaseAccordionCustom
 			:index="2"
 			:open="accordionSelect[1]"
 			:header-text="'Notas de Crédito'"
 			:header-desc="'Series para notas de crédito'"
-			:header-icon-color="'text-success'"
+			:header-icon-color="'text-warning'"
 			:open-accordion="showAccordion"
 		>
-			Notas de crédito
+			<div class="pb-4">
+				<div class="flex gap-3 py-3 px-4 text-xs text-[#A9803D] bg-[#FDEDD3] rounded-lg">
+					<UIcon
+						name="i-lucide-info"
+						class="size-6"
+					/>
+					<div>
+						<p class="mb-1">
+							Formato de series según SUNAT:
+						</p>
+						<ul class="list-disc ml-4 mb-1 space-y-0.5">
+							<li>
+								<p>Notas de Crédito para Facturas: FN + 2 dígitos (Ej: FN01)</p>
+							</li>
+							<li>
+								<p>Notas de Crédito para Boletas: BN + 2 dígitos (Ej: BN01)</p>
+							</li>
+						</ul>
+						<p>Nota: Las series y correlativos son únicos por sucursal.</p>
+						<p>Si creas otra sucursal, cambia el número: FN01 -> FN02, BN01 -> BN02, etc.</p>
+					</div>
+				</div>
+			</div>
 		</BaseAccordionCustom>
 		<BaseAccordionCustom
 			:index="3"
